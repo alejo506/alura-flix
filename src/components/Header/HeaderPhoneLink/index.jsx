30c7712c72b2c87eeb9937c83f4248c48287d488
Alejandro Corrales <alejo506a@gmail.com>
2024-12-19 01:44:39 -0600

    Button Home  custom

--- src/components/Header/HeaderPhoneLink/index.jsx
+++ src/components/Header/HeaderPhoneLink/index.jsx
@@ -15,9 +15,9 @@ const HeaderPhoneLink = ({ buttonStyles }) => {
                 <Link to="/">
                     <Button
                         variant="outlined"
-                        sx={buttonStyles("#2271D1", "#2271D1")}
+                        sx={{...buttonStyles("#2271D1", "#2271D1"), backgroundColor:"#000000E5", "&:hover":{backgroundColor:"#000000E5"}}}
                     >
-                        <img src="/icons/home-phone.svg" alt="Home" style={{ marginRight: "8px", backgroundColor:"#000000E5" }} />
+                        <img src="/icons/home-phone.svg" alt="Home" style={{ marginRight: "8px" }} />
                         HOME
                     </Button>
                 </Link>
