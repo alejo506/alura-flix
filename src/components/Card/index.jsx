d466467e70666c6802ec3f5d43fe9459eccf36d3
Alejandro <48256697+alejo506@users.noreply.github.com>
2026-02-12 14:40:32 -0600

    Change overflow property to visibility in header styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -46,7 +46,7 @@ const imgBorderStyles = ($categoryColor) => ({
 const headerStyles = ($categoryColor) => ({
   textAlign: "center",
   color: "#FFFFFF",
-  overflow: "hidden",
+  overflow: "visibility",
   padding: "15px",
   fontWeight: "bold",
   borderLeft: `4px solid ${$categoryColor}`,
